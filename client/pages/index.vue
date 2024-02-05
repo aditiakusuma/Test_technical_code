e452b4bfbef8b96eebf85883fe98c6b4af4536fb
aditiakusuma <aditiakusuma800@gmail.com>
2024-02-05 09:50:49 +0700

    lint

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,30 +1,33 @@
 <script setup lang="ts">
-    const input: Ref<null | number> = ref(null)
-    const result = ref()
-    const generate = async (type: 'odd' | 'prima' | 'triangle') => {
-        try {
-            const res = await $fetch(`/generate?type=${type}`, { baseURL: 'http://localhost:3333'})
-            result.value = res
-        } catch (error) {
-            
-        }
-    }
+const input: Ref<null | number> = ref(null);
+const result = ref();
+const generate = async (type: "odd" | "prima" | "triangle") => {
+  try {
+    const res = await $fetch(`/generate?type=${type}`, {
+      baseURL: "http://localhost:3333",
+    });
+    result.value = res;
+  } catch (error) {}
+};
 </script>
 
 <template>
-<div style=" padding: 80px;">
-    <input v-model="input" type="text" style=" margin-bottom: 20px" placeholder="Input Angka">
+  <div style="padding: 80px">
+    <input
+      v-model="input"
+      type="text"
+      style="margin-bottom: 20px"
+      placeholder="Input Angka"
+    />
     <div>
-        <button style="margin-right: 10px;" @click="generate('triangle')">
-            Generate Segitiga
-        </button>
-        <button style="margin-right: 10px;" @click="generate('odd')">
-            Generate Bilangan Ganjil
-        </button>
-        <button @click="generate('prima')">
-            Generate Bilangan Prima
-        </button>
+      <button style="margin-right: 10px" @click="generate('triangle')">
+        Generate Segitiga
+      </button>
+      <button style="margin-right: 10px" @click="generate('odd')">
+        Generate Bilangan Ganjil
+      </button>
+      <button @click="generate('prima')">Generate Bilangan Prima</button>
     </div>
-    <div>Result : {{  result }}</div>
-</div>
+    <div>Result : {{ result }}</div>
+  </div>
 </template>
